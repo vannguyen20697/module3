0230b73114ed0f6cf000f1c867dd5b638a1efaac
vannguyen20697 <110239577+vannguyen20697@users.noreply.github.com>
2022-12-18 01:08:36 +0700

    Add files via upload

---
+++ ss2_thiet_ke_va_tao_co_so_du_lieu/bai_tap/quan_ly_ban_hang.sql
@@ -0,0 +1,34 @@
+create database quan_ly_ban_hang;
+use quan_ly_ban_hang;
+create table customer(
+	c_ID int auto_increment primary key not null,
+    c_Name varchar(255) not null,
+    c_Age int not null
+    );
+    
+create table orders(
+    o_ID int not null auto_increment primary key,
+    c_ID int not null,
+    o_Date datetime not null,
+    o_TotalPrice int not null,
+    foreign key (c_ID) references customer (c_ID)
+    );
+    
+create table product(
+	p_ID int not null auto_increment primary key,
+    p_Name varchar(255) not null,
+    p_Price int not null
+    );
+    
+create table order_detail(
+	o_ID int not null,
+    p_ID int not null,
+    od_QTY int not null,
+    primary key (o_ID,p_ID),
+    foreign key (o_ID) 
+				references order_1(o_ID),
+    foreign key (p_ID) 
+				references product(p_ID)
+    );
+    
+    
