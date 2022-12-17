b6301165d2b8bc55b3a01fbd253ffa324c4e883d
vannguyen20697 <110239577+vannguyen20697@users.noreply.github.com>
2022-12-17 22:10:50 +0700

    Add files via upload

---
+++ ss1_co_so_du_lieu/quan_ly_sinh_vien.sql
@@ -0,0 +1,18 @@
+create database student_manager;
+use student_manager;
+create table student(
+id int,
+name varchar(255),
+age int,
+country varchar(255)
+);
+create table teacher(
+id int,
+name varchar(255),
+age int,
+country varchar(255)
+);
+create table class(
+id int,
+name varchar(255)
+); 
---
+++ ss1_co_so_du_lieu/student.sql
@@ -0,0 +1,8 @@
+-- Cau lenh tao database
+create database a07_student;
+-- chon DB Muon lam viec
+use a07_student;
+create table mon_hoc(
+ma_mh int,
+ten_mh varchar(255)
+);
